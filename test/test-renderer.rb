4e7b2bc13e71ca94363124ff77172063fd8caf33
Masafumi Yokoyama <myokoym@gmail.com>
2014-01-17 00:56:01 +0900

    test: indent

--- test/test-renderer.rb
+++ test/test-renderer.rb
@@ -3,39 +3,39 @@ require "cairo"
 
 class RendererTest < Test::Unit::TestCase
   class BaseTest < self
-  def setup
-    @renderer = TankaRenderer::Renderer::Image.new
-  end
-
-  class GuessFontTest < self
-    def test_found
-      part_of_name = "M"
-      assert_match(/#{part_of_name}/, @renderer.guess_font(part_of_name))
+    def setup
+      @renderer = TankaRenderer::Renderer::Image.new
     end
 
-    def test_not_found
-      part_of_name = "ABCDE12345"
-      assert_nil(@renderer.guess_font(part_of_name))
-    end
-  end
+    class GuessFontTest < self
+      def test_found
+        part_of_name = "M"
+        assert_match(/#{part_of_name}/, @renderer.guess_font(part_of_name))
+      end
 
-  class DrawTest < self
-    def test_ten_characters
-      assert_nothing_raised_in_draw("1234567890")
+      def test_not_found
+        part_of_name = "ABCDE12345"
+        assert_nil(@renderer.guess_font(part_of_name))
+      end
     end
 
-    private
-    def assert_nothing_raised_in_draw(text)
-      width =  200
-      height = 200
-      assert_nothing_raised do
-        Cairo::ImageSurface.new(:argb32, width, height) do |surface|
-          Cairo::Context.new(surface) do |context|
-            @renderer.draw(context, text)
+    class DrawTest < self
+      def test_ten_characters
+        assert_nothing_raised_in_draw("1234567890")
+      end
+
+      private
+      def assert_nothing_raised_in_draw(text)
+        width =  200
+        height = 200
+        assert_nothing_raised do
+          Cairo::ImageSurface.new(:argb32, width, height) do |surface|
+            Cairo::Context.new(surface) do |context|
+              @renderer.draw(context, text)
+            end
           end
         end
       end
     end
-  end
   end
 end
